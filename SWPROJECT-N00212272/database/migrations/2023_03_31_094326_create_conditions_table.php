30480a8bd1f466e377441375cf836c88e527724a
Kacper Agatowski <n00212272@iadt.ie>
2023-04-05 20:25:42 +0100

    added timestamps

--- SWPROJECT-N00212272/database/migrations/2023_03_31_094326_create_conditions_table.php
+++ SWPROJECT-N00212272/database/migrations/2023_03_31_094326_create_conditions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('conditions', function (Blueprint $table) {
             $table->id();
             $table->string('type');
+            $table->timestamps();
         });
     }
 
