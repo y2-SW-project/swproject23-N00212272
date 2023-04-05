7066a3262523899226baecc8a162079efdfa07e9
Kacper Agatowski <n00212272@iadt.ie>
2023-04-05 20:27:21 +0100

    added filter function

--- SWPROJECT-N00212272/resources/views/admin/products/index.blade.php
+++ SWPROJECT-N00212272/resources/views/admin/products/index.blade.php
@@ -2,18 +2,93 @@
 
 @section('content')
 <div class="container">
-  <a href="{{ route('admin.products.create') }}" class="btn-link btn-lg mb-2">+ New Product</a>
-</div>
-     <div class="container">
-	<div class="row">
-	 @forelse ($products as $product)
+<div class="d-flex justify-content-between">
+
+  <a href="{{ route('admin.products.create') }}" class="btn btn-lg bg-success mb-2">New Product</a>
     
-  <div class="col-4  mt-2">
+	
+  <a href="{{ route('admin.categories.index') }}" class="btn btn-lg bg-success mb-2">Categories</a>
+    
+
+    <a href="{{ route('admin.conditions.index') }}" class="btn btn-lg bg-success mb-2">Conditions</a>
+	 
+	  
+	  <a href="{{ route('admin.sizes.index') }}" class="btn btn-lg bg-success mb-2">Sizes</a>
+	    
+	    <a href="{{ route('admin.materials.index') }}" class="btn btn-lg bg-success mb-2">Materials</a>
+  
+  </div>
+  </div>
+<div class="container-fluid">
+<form action="" method="GET">
+<div class="row mt-3">
+<div class="col-md-2 col-12">
+<h2>Filter to your benefit</h2>
+<div class="col-md-10 ">
+<label>Filter by Category</label>
+<select name="category_id" class="form-select">
+	<option value="">Select category</option>
+	<option value="1" {{Request::get('category_id') == '1'}}>Clothes</option>
+	<option value="2" {{Request::get('category_id') == '2'}}>Accessories</option>
+	<option value="3" {{Request::get('category_id') == '3'}}>Water</option>
+
+<option value="4" {{Request::get('category_id') == '4'}}>Toys</option>
+
+<option value="5" {{Request::get('category_id') == '5'}}>Car</option>
+
+<option value="6" {{Request::get('category_id') == '6'}}>Nursery</option>
+</select>
+</div>
+<div class="col-md-10">
+<label>Filter by condition</label>
+<select name="condition_id" class="form-select">
+	<option value="">Select condition</option>
+	<option value="1" {{Request::get('condition_id') == '1'}}>Bad</option>
+	<option value="2" {{Request::get('condition_id') == '2'}}>Okay</option>
+<option value="3" {{Request::get('condition_id') == '3'}}>Good</option>
+<option value="4" {{Request::get('condition_id') == '4'}}>Excellent</option>
+</select>
+</div>
+<div class="col-md-10">
+<label>Filter by Size</label>
+<select name="size_id" class="form-select">
+	<option value="">Select category</option>
+	<option value="1" {{Request::get('size_id') == '1'}}>0-6 months</option>
+	<option value="2" {{Request::get('size_id') == '2'}}>6-12 months</option>
+	<option value="3" {{Request::get('size_id') == '3'}}>1+</option>
+	<option value="4" {{Request::get('size_id') == '4'}}>2+</option>
+	<option value="5" {{Request::get('size_id') == '5'}}>3+</option>
+	<option value="6" {{Request::get('size_id') == '6'}}>4+</option>
+	<option value="7" {{Request::get('size_id') == '7'}}>5+</option>
+</select>
+</div>
+<div class="col-md-10">
+<label>Filter by Price</label>
+<select name="price" class="form-select">
+	<option value="">Select price</option>
+	<option value="desc" {{Request::get('price') == 'desc'}}>highest->lowest</option>
+	<option value="asc" {{Request::get('price') == 'asc'}}>lowest->highest</option>
+</select>
+</div>
+
+<div class="col-md-10">
+<br/>
+<button type="submit" class="btn btn-primary">Filter</button>
+</div>
+</div>
+
+</form>
+	<div class="row col-md-8 col-12">
+	 @forelse ($products as $product)
+   
+  <div class="col-md-4 col-6 mt-2">
 	 <div class="card ">
 		 <a href="{{route('admin.products.show', $product) }}"><img src="{{ asset('storage/images/' . $product->image1) }}" 
-    class="card-img-top d-none d-md-block" height="250" alt="" />
+    class="card-img-top d-md-block d-none" height="250" alt="" />
+	 <a href="{{route('admin.products.show', $product) }}"><img src="{{ asset('storage/images/' . $product->image1) }}" 
+    class="card-img-top d-md-none d-block" height="150" alt="" />
 			<div class="card-body"></a>
-				<h4 class="card-title text-center ">{{($product->name)}}</h4>
+				<h4 class="card-title text-center d-none d-md-block ">{{($product->name)}}</h4>
 				<h5 class="card-subtitle text-center">Category: {{($product->category->name) }}</h5>
 				<h6 class="card-text text-center mt-2"><i class="fa-solid fa-location-pin px-2"></i>Price :£{{$product->price}}</h6>
 				<div class="row">
@@ -32,5 +107,7 @@
         @endforelse
 		</div>
 		</div>
+		</div>
+		
 		
 @endsection
