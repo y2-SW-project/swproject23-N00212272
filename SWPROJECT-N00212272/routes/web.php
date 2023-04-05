19241b503bdbf5b99bc8162a201d9e3273ddb765
Kacper Agatowski <n00212272@iadt.ie>
2023-04-05 20:29:31 +0100

    added routes for each model and user

--- SWPROJECT-N00212272/routes/web.php
+++ SWPROJECT-N00212272/routes/web.php
@@ -1,16 +1,30 @@
 <?php
 //VSC is told where to find the product controller
 //product controllers
-use App\Http\Controllers\Admin\ProductController as AdminProductController;
-use App\Http\Controllers\Customer\ProductController as CustomerProductController;
+use App\Models\Product;
+use App\Models\Material;
+use App\Models\Category;
+use App\Models\Size;
+use App\Models\Condition;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\Customer\ProductController as CustomerProductController;
+//Material controllers
+use App\Http\Controllers\Admin\MaterialController as AdminMaterialController;
+//Category controllers
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+//Condition controllers
+use App\Http\Controllers\Admin\ConditionController as AdminConditionController;
+//Size controllers
+use App\Http\Controllers\Admin\SizeController as AdminSizeController;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome.index');
 
 Auth::routes();
 
@@ -19,3 +33,14 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 
 Route::resource('/admin/products', AdminProductController::class)->middleware(['auth'])->names('admin.products');
+
+
+Route::resource('/customer/products', CustomerProductController::class)->middleware(['auth'])->names('customer.products');
+
+Route::resource('/admin/materials', AdminMaterialController::class)->middleware(['auth'])->names('admin.materials');
+
+Route::resource('/admin/categories', AdminCategoryController::class)->middleware(['auth'])->names('admin.categories');
+
+Route::resource('/admin/conditions', AdminConditionController::class)->middleware(['auth'])->names('admin.conditions');
+
+Route::resource('/admin/sizes', AdminSizeController::class)->middleware(['auth'])->names('admin.sizes');
